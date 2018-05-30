b2b29d22059a5d019b3707e35d529005c23d4862
Misael Pérez Chamorro <misaelpcgm@gmail.com>
2018-05-29 21:02:10 -0500

    Initial Commit

---
+++ ItemsStorage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ItemsStorage
+//
+//  Created by Misael Pérez Chamorro on 5/29/18.
+//  Copyright © 2018 Misael Pérez Chamorro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ItemsStorage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ItemsStorage
+//
+//  Created by Misael Pérez Chamorro on 5/29/18.
+//  Copyright © 2018 Misael Pérez Chamorro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
